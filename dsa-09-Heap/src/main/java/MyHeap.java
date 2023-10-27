59304d3a7853f64087f5449e42ca93bd5cdac99f
selimdemirer <selimmdemirerr@gmail.com>
2023-10-27 15:04:56 +0300

    My Own Heap structure is completed

--- dsa-09-Heap/src/main/java/MyHeap.java
+++ dsa-09-Heap/src/main/java/MyHeap.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class MyHeap {
 
     int[] items;
@@ -8,15 +10,11 @@ public class MyHeap {
         this.size = 0;
     }
 
-
-
     public int leftChildIndex(int index){ return index*2 +1;} // Apparent Index will be sent to this method and as a return I am gonna get the left child address
     public int rightChildIndex(int index){ return index*2 +2;} // Apparent Index will be sent to this method and as a return I am gonna get the right child address
     public int parentIndex(int index) { return (index-1)/2;} // Child Index will be sent to this method and as a return I am gonna get the parent address
-
     public boolean hasLeftChild(int index){ return  leftChildIndex(index)<size;}
     public boolean hasRightChild(int index){ return  rightChildIndex(index)<size;}
-
     public int largerChildIndex(int index) {
 
         if (!hasLeftChild(index)) return index; // no child
@@ -34,6 +32,64 @@ public class MyHeap {
         items[second]=temp;
     }
 
+    public boolean isValidParent(int index){
+        if (!hasLeftChild(index)) return true;
+        else {
+            boolean isValid=(items[index]>=items[leftChildIndex(index)]);
+            if (hasRightChild(index)) {
+                isValid= (items[index]>=items[leftChildIndex(index)]&&items[index]>=items[rightChildIndex(index)]);
+            }
+            return isValid;
+        }
+    }
+
+    public void insert(int value) {
+        if(size==items.length) throw new NoSuchElementException();
+        else {
+            items[size++]=value;
+            bubbleUp();
+        }
+    }
+
+    public void bubbleUp(){
+        //calculate start point/index
+        int index=size-1;
+        // while index >0 %% parent is less than child continue...
+        // 1. swap child and parent
+        // calculate next index = parent index
+        while (index>0 && items[index]>items[parentIndex(index)]){
+            swap (index, parentIndex(index));
+            // assign index with the parent index
+            index=parentIndex(index);//like current=current.next
+        }
+    }
+
+    public int remove(){
+        if (size==0) throw new NoSuchElementException();
+        else {
+            int result= items[0];
+            items[0]=items[--size];
+            bubbleDown();
+            return result;
+        }
+    }
+
+    public void bubbleDown(){
+        int index=0;
+        int largerChildIndex;
+        while (index<=size && !isValidParent(index)){
+            largerChildIndex=largerChildIndex(index);
+            swap(index, largerChildIndex);
+            index=largerChildIndex;
+        }
+    }
+
+    public void printHeap(){
+        for (int i = 0; i < size; i++) {
+            System.out.print(items[i]+ ", ");
+        }
+        System.out.println();
+    }
 
 
 }
