9a125d31a06e6564487095c2bfa707f0296536aa
selimdemirer <selimmdemirerr@gmail.com>
2023-10-31 16:20:56 +0300

    Search Module is created

--- dsa-09-Heap/src/main/java/KthLargest.java
+++ dsa-09-Heap/src/main/java/KthLargest.java
@@ -4,14 +4,18 @@ public class KthLargest {
         System.out.println(getKthLargest(numbers, 2));
     }
     public static int getKthLargest(int[] array, int k){
+
         // create a heap
         MyHeap heap=new MyHeap(array.length);
+        // put array into a heap
         for (int i = 0; i < array.length; i++) {
           heap.insert(array[i]);
         }
+        // remove root(because it is largest) k-1 times
         for (int i = 0; i <k-1 ; i++) {
             heap.remove();
         }
+        // return root
         return heap.peek();
     }
 }
